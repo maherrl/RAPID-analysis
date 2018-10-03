88fe6964538f8413241ead71c015c24cc057f2f8
maherrl <maherr@oregonstate.edu>
2018-10-03 13:34:07 -0700

    Simple normalization by proportion and commented out table export to QIIME2.

--- scripts/import_qiime_to_phyloseq.R
+++ scripts/import_qiime_to_phyloseq.R
@@ -3,7 +3,7 @@
 # to phyloseq for the RAPID data
 # Created by Rebecca Maher
 # Created on 9/20/18
-# Edited on 10/2/18
+# Edited on 10/3/18
 ########################################################
 
 # clear workspace-----------------------------
@@ -15,7 +15,6 @@ packageVersion('phyloseq')
 library(ape)
 library("biomformat");packageVersion("biomformat")
 
-setwd("~/Box Sync/RAPID-analysis/")
 
 ##
 # Importing data and transformations ##
@@ -45,21 +44,13 @@ samples <- sample_names(qd)
 samples_nona <- setdiff(samples, samples_wna)
 qd <- prune_samples(samples_nona, qd)
 
-# Normalization technique using Naive Proportions from the "Waste Not, Want Not"
-# paper
-# Define the naive (simple proportion) normalization function.
-# Normalize total sequences represented
-normf = function(x, tot=max(sample_sums(qd))){tot*x/sum(x)}
-qd = transform_sample_counts(qd, normf)
-# Scale by dividing each variable by its standard deviation
-qd = transform_sample_counts(qd,function(x) x/sd(x))
-# Center by subtracting the median
-# qd = transform_sample_counts(qd, function(x) x - median(x)) # didnt do anything?
+# Normalization technique by proportion
+qd = transform_sample_counts(qd, function(x) x/sum(x))
 
 # Export it to Qiime2
-otu <- as(otu_table(qd), "matrix")
-otu_biom <- make_biom(data=otu)
-write_biom(otu_biom, "/Users/Becca/Box Sync/RAPID/RAPID-analysis/data/otu-table-prop.biom")
+#otu <- as(otu_table(qd), "matrix")
+#otu_biom <- make_biom(data=otu)
+#write_biom(otu_biom, "/Users/Becca/Box Sync/RAPID/RAPID-analysis/data/otu-table-prop.biom")
 
 # Make a numeric factor into a categorical
 sample_data(qd)$time=factor(get_variable(qd,"time"))
@@ -67,5 +58,5 @@ sample_data(qd)$time=factor(get_variable(qd,"time"))
 
 ##
 # Save the Formal class phyloseq to an external file to load in other scripts (qd = qiimedata)
-save(qd, file = "~/Box Sync/RAPID/RAPID-analysis/data/qd.RData")
+save(qd, file = "~/Box Sync/RAPID/RAPID-analysis/data/qd_prop.RData")
 # The phyloseq object qd is now ready to use in diversity analyses
